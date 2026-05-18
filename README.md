df292b0fd272b7755a405ed0277963eda265203a
Freekers <1370857+Freekers@users.noreply.github.com>
2026-05-18 09:31:17 +0200

    Added compatibility for ComfoAir Controller from Svenar (not affiliated)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 _Based on the excellent original work from https://github.com/timjong93/lovelace-wtw._
 
-Use https://github.com/LukasdeBoer/esp8266-whr930-mqtt to connect your WHR930 to Homeassistant and then use this lovelace card to visualize your data!
+Use https://shop.svenar.nl/product/?p=esphome_whr930 to connect your WHR930 to Homeassistant and then use this lovelace card to visualize your data!
 
 ![Image](https://raw.githubusercontent.com/christiaangoossens/lovelace-wtw/master/result.png)
 
@@ -11,12 +11,13 @@ Use https://github.com/LukasdeBoer/esp8266-whr930-mqtt to connect your WHR930 to
 - Added support for installing through HACS
 - Added dark mode compatible version of the background
 - Replaced the mode indicator (fans in the middle) with just a number for better readability.
+- Added compatibility for ComfoAir Controller from Svenar (not affiliated)
 
 # Installation
 
 1. Install as a custom repository using HACS
 
-[![Open your Home Assistant instance and open a repository inside the Home Assistant Community Store.](https://my.home-assistant.io/badges/hacs_repository.svg)](https://my.home-assistant.io/redirect/hacs_repository/?owner=christiaangoossens&repository=lovelace-wtw&category=Plugin)
+[![Open your Home Assistant instance and open a repository inside the Home Assistant Community Store.](https://my.home-assistant.io/badges/hacs_repository.svg)](https://my.home-assistant.io/redirect/hacs_repository/?owner=freekers&repository=lovelace-wtw&category=Plugin)
 
 2. Download the version of your choice.
 3. Add a card of type `type: 'custom:wtw-card'` (YAML) or 'WTW Card' (UI picker) to your dashboard.
--- src/lovelace-wtw.js
+++ src/lovelace-wtw.js
@@ -1,240 +1,167 @@
-import { LitElement, html, css, unsafeCSS } from "lit-element";
-import bgImage from './wtw_heat.png';
-
-class WTWCard extends LitElement {
-  static get properties() {
-    return {
-      hass: {},
-      config: {},
-    };
+class WTWCard extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
   }
 
-  render() {
-    return html`
-    <ha-card>
-    <div class="container">
-      <div class="bg">
-          <div class="flex-container">
+  set hass(hass) {
+    this._hass = hass;
+    this._render();
+  }
+
+  _s(entity) {
+    return this._hass.states[entity]?.state ?? 'N/A';
+  }
+
+  _render() {
+    if (!this._hass) return;
+
+    const s = (e) => this._s(e);
+
+    const fanActive     = this._hass.states['binary_sensor.wtw_supply_fan_active']?.state === 'on';
+    const filterOk      = s('sensor.wtw_filter_status') === 'Ok';
+    const bypassActive  = this._hass.states['binary_sensor.wtw_bypass_open']?.state === 'on';
+    const valveState    = this._hass.states['sensor.wtw_preheating_valve']?.state;
+    const preHeatActive = valveState !== undefined && valveState !== 'Closed';
+    const summerMode    = this._hass.states['binary_sensor.wtw_summer_mode']?.state === 'on';
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        .container { padding: 10px; }
+        .bg {
+          background-image: url("/local/community/lovelace-wtw/wtw_heat.png");
+          height: 200px;
+          background-size: contain;
+          background-repeat: no-repeat;
+          background-position-y: center;
+        }
+        .flex-container {
+          display: flex;
+          justify-content: space-between;
+          height: 100%;
+        }
+        .flex-col-main {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          padding: 30px 0px;
+          font-size: large;
+          text-align: center;
+          font-weight: bold;
+        }
+        .flex-col-out {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+        .flex-col-in {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+        .fan-state { padding-top: 15px; }
+        .info-row {
+          background: rgba(0, 0, 0, 0.2);
+          margin-top: 10px;
+          padding: 5px;
+          border-top: rgba(0, 0, 0, 0.4);
+          -webkit-box-shadow: 0px -4px 3px rgba(50, 50, 50, 0.75);
+          -moz-box-shadow: 0px -4px 3px rgba(50, 50, 50, 0.75);
+          box-shadow: 0px -2.5px 3px rgba(0, 0, 0, 0.4);
+          display: flex;
+          justify-content: space-around;
+        }
+        .inactive { opacity: 0.7; }
+        .warning  { color: #d80707db; }
+      </style>
+      <ha-card>
+        <div class="container">
+          <div class="bg">
+            <div class="flex-container">
+
               <div class="flex-col-out">
-                  <div>${
-                    this.hass.states["sensor.wtw_outside_temperature"].state
-                  }°C</div>
-                  <div class="fan-state"><ha-icon icon="mdi:fan"></ha-icon></ha-icon> ${Math.trunc(
-                    this.hass.states["sensor.wtw_intake_fan_speed"].state
-                  )}%</div>
-                  <div>${
-                    this.hass.states["sensor.wtw_exhaust_temperature"].state
-                  }°C</div>
-                  <div class="fan-state"><ha-icon icon="mdi:fan"></ha-icon> ${Math.trunc(
-                    this.hass.states["sensor.wtw_exhaust_fan_speed"].state
-                  )}%</div>
+                <div>Outside: ${s('sensor.wtw_outside_temperature')}°C</div>
+                <div class="fan-state">
+                  <ha-icon icon="mdi:fan"></ha-icon>
+                  ${Math.trunc(Number(s('sensor.wtw_intake_fan_rpm')))} RPM
+                </div>
+                <div>Exhaust: ${s('sensor.wtw_exhaust_temperature')}°C</div>
+                <div class="fan-state">
+                  <ha-icon icon="mdi:fan"></ha-icon>
+                  ${Math.trunc(Number(s('sensor.wtw_exhaust_fan_rpm')))} RPM
+                </div>
               </div>
+
               <div class="flex-col-main">
-                  <div>${Math.trunc(
-                    this.hass.states["sensor.wtw_comfort_temperature"].state
-                  )}°C</div>
-                  <div>${Math.trunc(
-                    this.hass.states["sensor.wtw_ventilation_level"].state
-                  )}</div>
+                <div><ha-icon icon="mdi:home-thermometer"></ha-icon>
+                  ${this._hass.states['climate.wtw_comfoair']?.attributes?.temperature ?? 'N/A'}°C</div>
+                <div><ha-icon icon="mdi:fan"></ha-icon>
+                  ${Math.trunc(Number(s('sensor.wtw_ventilation_level')))}</div>
               </div>
+
               <div class="flex-col-in">
-                  <div>${
-                    this.hass.states["sensor.wtw_return_temperature"].state
-                  }°C</div>
-                  <div class="fan-state"><ha-icon icon="mdi:fan"></ha-icon> ${Math.trunc(
-                    this.hass.states["sensor.wtw_return_air_level"].state
-                  )}%</div>
-                  <div>${
-                    this.hass.states["sensor.wtw_supply_temperature"].state
-                  }°C</div>
-                  <div class="fan-state"><ha-icon icon="mdi:fan"></ha-icon> ${Math.trunc(
-                    this.hass.states["sensor.wtw_supply_air_level"].state
-                  )}%</div>
+                <div>Return: ${s('sensor.wtw_return_temperature')}°C</div>
+                <div class="fan-state">
+                  <ha-icon icon="mdi:fan"></ha-icon>
+                  ${Math.trunc(Number(s('sensor.wtw_return_air_level')))}%
+                </div>
+                <div>Supply: ${s('sensor.wtw_supply_temperature')}°C</div>
+                <div class="fan-state">
+                  <ha-icon icon="mdi:fan"></ha-icon>
+                  ${Math.trunc(Number(s('sensor.wtw_supply_air_level')))}%
+                </div>
               </div>
+
+            </div>
           </div>
-      </div>
-      </div>
-      <div class="info-row">
-      ${this.getFanTmpl()}
-      ${this.getAirFilterTmpl()}
-      ${this.getBypassTmpl()}
-      ${this.getPreHeatTmpl()}
-      ${this.getSummerModeTmpl()}
-      </div>
-    </ha-card>  
+        </div>
+        <div class="info-row">
+          <ha-icon
+            title="${fanActive ? 'Intake Fan Active' : 'Intake Fan Inactive'}"
+            class="${fanActive ? '' : 'inactive'}"
+            icon="mdi:fan">
+          </ha-icon>
+          <ha-icon
+            title="${filterOk ? 'Filters are OK' : 'Filter Warning!'}"
+            class="${filterOk ? '' : 'warning'}"
+            icon="mdi:air-filter">
+          </ha-icon>
+          <ha-icon
+            title="${bypassActive ? 'Bypass Active' : 'Bypass Inactive'}"
+            class="${bypassActive ? '' : 'inactive'}"
+            icon="mdi:electric-switch">
+          </ha-icon>
+          <ha-icon
+            title="${preHeatActive ? 'Valve Preheating Active' : 'Valve Preheating Inactive'}"
+            class="${preHeatActive ? '' : 'inactive'}"
+            icon="mdi:radiator">
+          </ha-icon>
+          <ha-icon
+            title="${summerMode ? 'Summer Mode' : 'Winter Mode'}"
+            class="${summerMode ? 'inactive' : ''}"
+            icon="${summerMode ? 'mdi:weather-sunny' : 'mdi:snowflake'}">
+          </ha-icon>
+        </div>
+      </ha-card>
     `;
-  }
-
-  getFanTmpl() {
-    if (this.hass.states["sensor.wtw_intake_fan_active"].state == "Yes") {
-      return html`<ha-icon title="Intake Fan Active" icon="mdi:fan"></ha-icon>`;
-    } else {
-      return html`<ha-icon
-        title="Intake Fan Inactive"
-        class="inactive"
-        icon="mdi:fan"
-      ></ha-icon>`;
-    }
-  }
-
-  getAirFilterTmpl() {
-    if (this.hass.states["sensor.wtw_filter_status"].state == "Ok") {
-      return html`<ha-icon
-        title="Filters are OK"
-        icon="mdi:air-filter"
-      ></ha-icon>`;
-    } else {
-      return html`<ha-icon
-        title="Filter Warning!"
-        class="warning"
-        icon="mdi:air-filter"
-      ></ha-icon>`;
-    }
-  }
-
-  getBypassTmpl() {
-    if (Number(this.hass.states["sensor.wtw_bypass_factor"].state) > 0) {
-      return html`<ha-icon
-        title="Bypass Active"
-        icon="mdi:electric-switch"
-      ></ha-icon>`;
-    } else {
-      return html`<ha-icon
-        title="Bypass Inactive"
-        class="inactive"
-        icon="mdi:electric-switch"
-      ></ha-icon>`;
-    }
-  }
-
-  getPreHeatTmpl() {
-    if (Number(this.hass.states["sensor.wtw_valve_preheating"].state) > 0) {
-      return html`<ha-icon
-        title="Valve Preheating Active"
-        icon="mdi:radiator"
-      ></ha-icon>`;
-    } else {
-      return html`<ha-icon
-        title="Valve Preheating Inactive"
-        class="inactive"
-        icon="mdi:radiator"
-      ></ha-icon>`;
-    }
-  }
-
-  getSummerModeTmpl() {
-    if (this.hass.states["sensor.wtw_summer_mode"].state == "No") {
-      return html`<ha-icon title="Winter Mode" icon="mdi:snowflake"></ha-icon>`;
-    } else {
-      return html`<ha-icon
-        title="Summer Mode"
-        class="inactive"
-        icon="mdi:weather-sunny"
-      ></ha-icon>`;
-    }
   }
 
   setConfig(config) {
     this.config = config;
   }
 
-  // The height of your card. Home Assistant uses this to automatically
-  // distribute all cards over the available columns.
   getCardSize() {
     return 7;
   }
-
-  static get styles() {
-    return css`
-      .container {
-        padding: 10px;
-      }
-      .bg {
-        background-image: url("${unsafeCSS(bgImage)}");
-        height: 200px;
-        background-size: contain;
-        background-repeat: no-repeat;
-        background-position-y: center;
-      }
-      .not-found {
-        background-color: yellow;
-        font-family: sans-serif;
-        font-size: 14px;
-        padding: 8px;
-      }
-      .flex-container {
-        display: flex;
-        justify-content: space-between;
-        height: 100%;
-      }
-      .flex-col-main {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        padding: 30px 0px;
-        font-size: x-large;
-        text-align: center;
-        font-weight: bold;
-      }
-      .flex-col-out {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-      }
-      .flex-col-in {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-      }
-      .fan-state {
-        padding-top: 15px;
-      }
-      .spin {
-        animation-name: spin;
-        animation-duration: 2000ms;
-        animation-iteration-count: infinite;
-        animation-timing-function: linear;
-      }
-
-      .info-row {
-        background: rgba(0, 0, 0, 0.2);
-        margin-top: 10px;
-        padding: 5px;
-        border-top: rgba(0, 0, 0, 0.4);
-        -webkit-box-shadow: 0px -4px 3px rgba(50, 50, 50, 0.75);
-        -moz-box-shadow: 0px -4px 3px rgba(50, 50, 50, 0.75);
-        box-shadow: 0px -2.5px 3px rgba(0, 0, 0, 0.4);
-        display: flex;
-        justify-content: space-around;
-      }
-
-      .inactive {
-        opacity: 0.7;
-      }
-
-      .warning {
-        color: #d80707db;
-      }
-
-      @keyframes spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-    `;
-  }
 }
 
-console.info("Loaded WTW card.");
+console.info("Loaded ComfoAir Controller WTW card.");
 
 window.customCards = window.customCards || [];
 window.customCards.push({
   type: 'wtw-card',
-  name: 'WTW Card',
-  description: 'WTW Card (specifically works with WHR 930 entities)',
+  name: 'ComfoAir Controller WTW Card',
+  description: 'ComfoAir Controller WTW Card (specifically works with WHR 930 entities)',
 });
 
-customElements.define("wtw-card", WTWCard);
+customElements.define('wtw-card', WTWCard);
